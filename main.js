93269184fee4bcb9bae5baec23f2e1af5032dc77
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-03-27 16:03:51 +0530

    select all id  of table

---
+++ main.js
@@ -0,0 +1,8 @@
+function calculate() {
+  var amount = document.querySelector("#amount");
+  var apr = document.querySelector("#apr");
+  var year = document.querySelector("#year");
+  var zipcode = document.querySelector("#zipcode");
+  var output = document.querySelector("#output");
+  var totalinterest = document.querySelector("#totalinterest");
+}
