5ee27857510e0e1aac4931a5c76d431a90bb6b76
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-04-01 22:12:51 +0530

    monthly payment fig.

--- main.js
+++ main.js
@@ -5,4 +5,11 @@ function calculate() {
   var zipcode = document.querySelector("#zipcode");
   var output = document.querySelector("#output");
   var totalinterest = document.querySelector("#totalinterest");
+
+  var principal = parseFloat(amount.value);
+  var interest = parseFloat(apr.value) / 100 / 12;
+  var payments = parseFloat(years.value) * 12;
+
+  var x = Math.pow(1 + interest, payments);
+  var monthly = (principal * x * interest) / (x - 1);
 }
